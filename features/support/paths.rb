1dcb941a67816831f80586a0a4808c38bb9f4ce2
Peter Klipfel <peter@klipfel.me>
2013-10-23 21:01:16 -0600

    testing navigation for creating a new movie

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,8 +13,9 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
+    when /^the (RottenPotatoes )?home(\s?)page$/ then '/movies'
     when /^the movies page$/ then '/movies'
+    when /^the (C|c)reate (M|m)ovie (P|p)age$/ then '/movies/new'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
