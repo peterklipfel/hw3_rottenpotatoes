722ed1abeb7a3402eeee678e8020423788e7b4ab
Peter Klipfel <peter@klipfel.me>
2013-10-31 20:17:11 -0600

    navigation passes without specific step definitions

--- features/navigation.feature
+++ features/navigation.feature
@@ -1,11 +1,11 @@
 Feature: create new movies
 
 Scenario: Creating a movie starting from the home page
-  Given I am on the RottenPotatoes homepage, 
-  When  I follow Add New Movie,  
-  Then  I should be on the Create Movie page. 
-  When  I fill in Title with Men in Black,  
-  And   I select PG-13 from Rating, 
-  And   I press Save Changes,  
-  Then  I should be in the RottenPotatoes home page, 
-  And   I should see Men in Black
+  Given I am on the RottenPotatoes homepage
+  When  I follow "Add New Movie"
+  Then  I should be on the Create Movie page
+  When  I fill in "Title" with "Men in Black"
+  And   I select "PG-13" from "Rating"
+  And   I press "Save Changes"
+  Then  I should be on the RottenPotatoes homepage
+  And   I should see "Men in Black"
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -41,9 +41,9 @@ When /^(.*) within (.*[^:]):$/ do |step, parent, table_or_string|
   with_scope(parent) { When "#{step}:", table_or_string }
 end
 
-# Given /^(?:|I )am on (.+)$/ do |page_name|
-#   visit path_to(page_name)
-# end
+Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
 
 When /^(?:|I )go to (.+)$/ do |page_name|
   visit path_to(page_name)
@@ -57,9 +57,9 @@ When /^(?:|I )follow "([^"]*)"$/ do |link|
   click_link(link)
 end
 
-# When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
-#   fill_in(field, :with => value)
-# end
+When /^(?:|I )fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
+end
 
 When /^(?:|I )fill in "([^"]*)" for "([^"]*)"$/ do |value, field|
   fill_in(field, :with => value)
@@ -82,9 +82,9 @@ When /^(?:|I )fill in the following:$/ do |fields|
   end
 end
 
-# When /^(?:|I )select "([^"]*)" from "([^"]*)"$/ do |value, field|
-#   select(value, :from => field)
-# end
+When /^(?:|I )select "([^"]*)" from "([^"]*)"$/ do |value, field|
+  select(value, :from => field)
+end
 
 When /^(?:|I )check "([^"]*)"$/ do |field|
   check(field)
@@ -227,14 +227,14 @@ Then /^the "([^"]*)" checkbox(?: within (.*))? should not be checked$/ do |label
   end
 end
  
-# Then /^(?:|I )should be on (.+)$/ do |page_name|
-#   current_path = URI.parse(current_url).path
-#   if current_path.respond_to? :should
-#     current_path.should == path_to(page_name)
-#   else
-#     assert_equal path_to(page_name), current_path
-#   end
-# end
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
+  end
+end
 
 Then /^(?:|I )should have the following query string:$/ do |expected_pairs|
   query = URI.parse(current_url).query
